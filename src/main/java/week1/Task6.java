046cf2c55de14b32bd21e485d09ff4cb31d440e5
Denys <denyspohribnyi@gmail.com>
2021-09-12 22:50:32 +0300

    10

--- src/main/java/week1/Task6.java
+++ src/main/java/week1/Task6.java
@@ -1,27 +1,22 @@
 package week1;
-
-import java.util.Random;
-
 public class Task6 {
     //Написать программу, которая подсчитывает количество элементов массива int*ов,
     //которые делятся на 2, но не делятся на 3 и выводит в консоль
     public static void main(String[] args) {
         System.out.println("Task 6");
-        /*
-        * Есть числовой массив -  Создать массив
-        * Числа в массиве должны делиться на 2, при этом не делятся на 3
-        * Выводить в консоль эти числа
-        *
-        *  */
-        Random random = new Random();
-        int[] arr = new int[15];
-        for (int i = 0; i < arr.length; i++) {
-            int rnd = random.nextInt(100);
-            arr[i] = rnd;
+        int[] arr = {1,4,6,7,8,9,10,12,13,14,15,16};
+        for (int i = 0; i < arr.length; i++) {                  // выведем в консоль наш массив
+            System.out.print(arr[i]+ " ");
         }
-        for (int i = 0; i < arr.length; i++) {  // Просто распечатаем созданный массив
-            System.out.print(arr[i]+" ");
+        System.out.println();
+        System.out.println("Numbers that can be divided on 2 but not 3 :");
+        for (int i = 0; i < arr.length; i++) {                  // Пробежимся по массиву анализируя каждую ячейку
+            if(arr[i] % 2 == 0 && arr[i] % 3 != 0){             // В условии используем деление по модулю -
+                System.out.print(arr[i]+" ");                   // Если тру в обоих случаях (на 2 да, на 3 нет) то
+            }                                                   // печатаем значение
         }
-
     }
 }
+//1 4 6 7 8 9 10 12 13 14 15 16
+//Numbers that can be divided on 2 but not 3 :
+//4 8 10 14 16
