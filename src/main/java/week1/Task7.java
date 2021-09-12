d1603cf0f8c43fd3798bdc2445be1e4ef82b9dbf
Denys <denyspohribnyi@gmail.com>
2021-09-12 19:17:25 +0300

    9

--- src/main/java/week1/Task7.java
+++ src/main/java/week1/Task7.java
@@ -6,21 +6,31 @@ public class Task7 {
     //Написать программу, которая переворачивает массив
     public static void main(String[] args) {
         System.out.println("Task 7");
-        int[] arr = new int[10];
-        Random random = new Random();                           // Создадим массив и заполним его случайными числами
-        for (int i = 0; i < arr.length; i++) {
-            int rnd = random.nextInt(20);
-            arr[i] = rnd;
-        }
+        int[] arr = {1,2,3,4,5,6,7,8,9};
         for (int i = 0; i < arr.length; i++) {                  // Отобразим в консоли
             System.out.print(arr[i] + " ");
         }
-        int a = arr[0];
-        int b = arr.length-1;
-
         System.out.println();
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]+" ");
+        reverse(arr);
+        System.out.println("Reversed array is: ");
+        for (int i = 0; i < arr.length; i++) {                  // Отобразим в консоли
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+    public static void reverse(int[] arr){
+        int a = 0;
+        int b = arr.length-1;
+        while(a < b ){
+            int tmp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tmp;
+        a++;
+        b--;
         }
     }
 }
+//Task 7
+//1 2 3 4 5 6 7 8 9
+//Reversed array is:
+//9 8 7 6 5 4 3 2 1
