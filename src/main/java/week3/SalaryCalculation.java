64f27df4b609f0665ffa5d9e0ff2e0bc1de6ce90
Denys <denyspohribnyi@gmail.com>
2021-09-25 19:55:18 +0300

    18

--- src/main/java/week3/SalaryCalculation.java
+++ src/main/java/week3/SalaryCalculation.java
@@ -3,9 +3,13 @@ package week3;
 public class SalaryCalculation {
     public static void main(String[] args) {
         SalesAgentSalary agent1Junior = new SalesAgentSalary(160, 1);
-        SalesAgentSalary agent2Junior = new SalesAgentSalary(190,1);
+        agent1Junior.getSalary();
+        SalesAgentSalary agent2Junior = new SalesAgentSalary(190, 1);
+        agent2Junior.getSalary();
         SalesAgentSalary agent3Senior = new SalesAgentSalary(150, 1, 3, 10, 10000);
+        agent3Senior.getSalary();
         SalesAgentSalary agent4Senior = new SalesAgentSalary(180, 1, 4, 20, 15000);
+        agent4Senior.getSalary();
 
         //OUTPUT :
 //Salary of this junior agent is : $160.0
--- src/main/java/week3/SalesAgentSalary.java
+++ src/main/java/week3/SalesAgentSalary.java
@@ -1,16 +1,26 @@
 package week3;
 
 public class SalesAgentSalary {
+    double salary; // Переменная для геттеров\сеттеров
+    String status; // Определим в эту переменную статус(испыталки) агента, в завимости от конструктора
+
+    public double getSalary() {
+        System.out.println("Salary of this " + status + " agent is : $" + salary);
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
     public SalesAgentSalary(int hours, double rate) {
-        // double juniorRate = 1; - Если испытательный срок то можно установить фиксированную ставку для джуна
-        double setSalary = calcSalary(hours, rate);
-        System.out.println("Salary of this junior agent is : $" + setSalary);
+        setSalary(calcSalary(hours, rate));
+        status = "Junior";
     }
 
     public SalesAgentSalary(int hours, double rate, int exp, int sales, double resultOfSales) { // rate кажется, лишний параметр
-        double setSalary = calcSalary(hours, baseCoeff(exp)) + bonusOrFine(sales) + overSales(resultOfSales);
-        System.out.println("Salary of this senior agent is : $" + setSalary);
+        setSalary(calcSalary(hours, baseCoeff(exp)) + bonusOrFine(sales) + overSales(resultOfSales));
+        status = "Senior";
     }
 
     public int calcSalary(int hours, double rate) {
@@ -21,8 +31,8 @@ public class SalesAgentSalary {
             bonusOvertime = hours - 160;
             bonusCalculated = bonusOvertime * 1.5;
         }
-        double dSalary = (hours * dRate) + bonusCalculated; //часы*рейт // преобразование типа инт в дабл
-        Integer salary = (int) dSalary; // преобразование дабл в инт
+        double dSalary = (hours * dRate) + bonusCalculated;
+        Integer salary = (int) dSalary;
         return salary;
     }
 
@@ -60,45 +70,3 @@ public class SalesAgentSalary {
         return result;
     }
 }
-
-
-
-
-/*
-1 - Расчёт зарплаты агента по продажам
-Расчёт зарплаты нужно разбить на несколько методов:
-1. Первый метод получает количество отработанных часов и базовую ставку.
-если количество отработанных часов больше 160 то каждый
-последующий час по коэф 1,5 (параметры у метода - входящие часы
-int, и double rate) , возвращаемое значение - зарплата. Это должна
-быть сумма базовой зарплаты (до 160 часов) + бонус за переработку
-свыше 160 часов (если переработка была) (double)
-2. Второй метод - получает коэффициент на базовые часы (которые до 160) в
-зависимости от стажа работы (параметры у метода - стаж работы int,
-возвращаемое значение double коэффициент)
-до 2 лет 1
-от 2 до 4 - 1.2
-от 4 до 6 - 1.3
-от 6 лет - 1.4
-3. В зависимости от количества продаж - больше 20 - бонус 250, меньше 10
-штраф 150 (параметры у метода - количество продаж int, возвращаемое
-значение double бонус за количество продаж)
-4. четвертый в зависимости от суммы продаж больше 15000 $ - тоже добавляется
-бонус (параметры у метода - сумма продаж double, возвращаемое значение
-double бонус за сумму продаж)
--------------------------------------------------------------------------------------------
-Пункты 2, 3 и 4 появляются только при прохождении испытательного срока
-Создать класс SalesAgentSalary - у него будет 2 параметризированных конструктора -
-первый принимает в себя параметры - входящие часы int, double - рейт в час;
-второй принимает в себя параметры - входящие часы int, double - рейт в час, int - стаж
-работы, int - количество продаж, double - Сумма продаж
-Все методы расчета (можно взять с прошлого урока)
-Метод getSalary - который будет возвращать double
-В конструкторах вызывать метод setSalary - который будет сэтить сэлэри в
-зависимости от конструктора (и от того прошел ли испытательный срок
-соответственно)
-Понимание прошел ли сотрудник испытательный срок реализовать с помощью
-конструктора (если 2 параметра, то еще на ИС, если 5 параметров, то уже прошел)
-Создать класс SalaryCalculation.
-В нем создать метод main, в методе main 2 ссылки на объект SalesAgentSalary c
-разными конструкторами вывести в консоль результат*/
