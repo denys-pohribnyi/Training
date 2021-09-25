34464333f850f4905a82b93a8dd48ee6c16a4390
Denys <denyspohribnyi@gmail.com>
2021-09-25 18:24:01 +0300

    17

--- src/main/java/week3/SalaryCalculation.java
+++ src/main/java/week3/SalaryCalculation.java
@@ -2,8 +2,16 @@ package week3;
 
 public class SalaryCalculation {
     public static void main(String[] args) {
-        SalesAgentSalary agent1Junior = new SalesAgentSalary(170, 1);
-        SalesAgentSalary agent2Senior = new SalesAgentSalary(150, 1, 3, 10, 14000);
+        SalesAgentSalary agent1Junior = new SalesAgentSalary(160, 1);
+        SalesAgentSalary agent2Junior = new SalesAgentSalary(190,1);
+        SalesAgentSalary agent3Senior = new SalesAgentSalary(150, 1, 3, 10, 10000);
+        SalesAgentSalary agent4Senior = new SalesAgentSalary(180, 1, 4, 20, 15000);
+
+        //OUTPUT :
+//Salary of this junior agent is : $160.0
+//Salary of this junior agent is : $235.0
+//Salary of this senior agent is : $180.0
+//Salary of this senior agent is : $764.0
     }
 }
 // Во втором способе рассчета есть передаваемый параметр rate, он кажется лишним
